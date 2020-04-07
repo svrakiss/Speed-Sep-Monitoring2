47171d5b9e4c7fe27898041573c193939967973e
PhysicsJake <jrsacdcaddict@gmail.com>
2020-04-07 15:47:13 -0400

    updates

--- SSMdatacall.m
+++ SSMdatacall.m
@@ -5,7 +5,10 @@ function [outputArg1,outputArg2] =SSMdatacall()
 %   variables needed for processing by the Simulink model. Add subscriber
 %   notes in the same fashion to call any more data from the
 %   ROS2-data-analytics package output.
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+
+% Variables currently needed by simulink model: tsys, xc, yc, zc ,vxc, vyc, vzc
+% The ROS2 messages that bring these variables are: builtin_interfaces/Time
+% for tsys, geometry_msgs/Point for vxc, vyc, and vzc, and
+% geometry_msgs/Point32 to derive xc, yc, zc from for the bounding boxes.
 end
 
