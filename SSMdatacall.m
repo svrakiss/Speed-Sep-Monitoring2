7a3cdc6bc68efe6bfb85ad171ca06017f7f280de
PhysicsJake <jrsacdcaddict@gmail.com>
2020-04-08 13:33:19 -0400

    Rough completion of data transformation

--- SSMdatacall.m
+++ SSMdatacall.m
@@ -1,5 +1,4 @@
-function [xc,yc,zc,vxc,vyc,vzc] =SSMdatacall()
-% The function is intended to be a constant communication call between ROS2
+% This script is intended to be a constant communication call between ROS2
 % and Simulink.
 %   This function subscribes to the nodes and writes the data into the
 %   variables needed for processing by the Simulink model. Add subscriber
@@ -12,9 +11,14 @@ i = 0
 poseLocalData = receive(localization,10)
 poseMovementData = receive(movement,10)
 vxc = poseMovementData.objects.velocity.x;
+vxc = double(vxc);
 vyc = poseMovementData.objects.velocity.y;
+vyc = double(vyc);
 vzc = poseMovementData.objects.velocity.z;
+vzc = double(vzc);
 xc  = poseMovementData.objects.max.x;
+xc = double(xc);
 yc  = poseMovementData.objects.max.y;
-zc  = poseMovementData.objects.max.c;
-
+yc = double(yc);
+zc  = poseMovementData.objects.max.z;
+zc = double(zc);
--- ssmCallback.m
+++ ssmCallback.m
@@ -1,4 +1,3 @@
 function ssmCallback(message)
 global msg
-msg =addsample(msg,'Data',message.header.stamp.nanosec,'Time',now);
 end
