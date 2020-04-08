d7fb77f21013280a318aa597f676ed8eef4bc1e4
PhysicsJake <jrsacdcaddict@gmail.com>
2020-04-08 11:08:18 -0400

    Updated model and encompassing functions

--- SSMdatacall.m
+++ SSMdatacall.m
@@ -1,4 +1,4 @@
-function [outputArg1,outputArg2] =SSMdatacall()
+function [xc,yc,zc,vxc,vyc,vzc] =SSMdatacall()
 % The function is intended to be a constant communication call between ROS2
 % and Simulink.
 %   This function subscribes to the nodes and writes the data into the
@@ -6,16 +6,15 @@ function [outputArg1,outputArg2] =SSMdatacall()
 %   notes in the same fashion to call any more data from the
 %   ROS2-data-analytics package output.
 
-% Variables currently needed by simulink model: tsys, xc, yc, zc ,vxc, vyc, vzc
-
-% The ROS2 messages that bring these variables are: 
-
-% builtin_interfaces/Time for tsys
-
-
-% geometry_msgs/Point for vxc, vyc, and vzc
-
-
-% geometry_msgs/Point32 to derive xc, yc, zc from for the bounding boxes.
-end
+% Variables currently needed by simulink model: xc, yc, zc ,vxc, vyc, vzc
+% For first run purposes, position will be max position of bounding box.
+i = 0
+poseLocalData = receive(localization,10)
+poseMovementData = receive(movement,10)
+vxc = poseMovementData.objects.velocity.x;
+vyc = poseMovementData.objects.velocity.y;
+vzc = poseMovementData.objects.velocity.z;
+xc  = poseMovementData.objects.max.x;
+yc  = poseMovementData.objects.max.y;
+zc  = poseMovementData.objects.max.c;
 
