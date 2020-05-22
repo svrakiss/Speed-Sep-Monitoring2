c3194835529f3b8c319209b512b023b9891d7c82
PhysicsJake <jrsacdcaddict@gmail.com>
2020-05-22 05:45:46 -0400

    New demo, comments, and loose ends

--- DataLoop.m
+++ DataLoop.m
@@ -1,4 +1,27 @@
-i = 0;
+% This function loops the Simulink model for 100 loops and outputs 100
+% Separation distances. It has the constants given for our robot as well as
+% the other constants necessary. It calls the SSMdatacall function to bring
+% the values from ROS2.
+%
+%
+%
+%**************************************************************************
+format long g
+t_current = 0;
+% i is a looping variable
+i = 1;
+% Ts is the stopping time of the robot
+Ts = 1;
+% Tr is the reaction time of the robot system
+Tr = 1;
+% Vs is the velocity of the robot when stopping initializes
+Vs = 1;
+% C is the instusion distance of a human into the separation zone
+C = 1;
+% Zd is the position uncertaitnty of a human
+Zd = 1;
+% Zr is the position uncertainty of the robot
+Zr = 1;
 while i < 100;
 [xc,yc,zc,vxc,vyc,vzc,time,tn] = SSMdatacall(movement);
 sim('iso_calc_temp_ver3.slx');
--- SSMdatacall.m
+++ SSMdatacall.m
@@ -1,13 +1,14 @@
 function [xc,yc,zc,vxc,vyc,vzc,time,tn] = SSMdatacall(movement)
 % This script is intended to be a constant communication call between ROS2
 % and Simulink.
-%   This function subscribes to the nodes and writes the data into the
+%   This function recieves the data form the movement topic and writes the data into the
 %   variables needed for processing by the Simulink model. Add subscriber
-%   notes in the same fashion to call any more data from the
+%   nodes for respective topics in the same fashion to call any more data from the
 %   ROS2-data-analytics package output.
 
 % Variables currently needed by simulink model: xc, yc, zc ,vxc, vyc, vzc
 % For first run purposes, position will be max position of bounding box.
+% The variables need to be converted into doubles for Simulink.
 
 %poseLocalData = receive(localization,10)
 poseMovementData = receive(movement,10);
@@ -23,6 +24,7 @@ yc  = poseMovementData.objects(1).max.y;
 yc = double(yc);
 zc  = poseMovementData.objects(1).max.z;
 zc = double(zc);
+% The time variables are included for future synchronization and indexing.
 time = poseMovementData.header(1).stamp.sec;
 time = double(time);
 tn = poseMovementData.header(1).stamp.nanosec;
